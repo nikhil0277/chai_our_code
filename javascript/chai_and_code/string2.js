03ca9b182b4b673efc5e9068870750de7e04a48a
nikhil0277 <nikhil07407@gmial.com>
2024-06-21 21:26:03 +0530

    commit

---
+++ javascript/chai_and_code/string2.js
@@ -0,0 +1,20 @@
+let name ="nikhil"
+let studentOf ="sgi"
+console.log(`Hi I am ${name} and student of ${studentOf}`)
+const gameName=new String ("free fire")
+console.log( typeof gameName)
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('i'))
+console.log(gameName.substring(0,8))
+console.log(gameName.slice(-8,8))
+const classs="    b.tech cse     "
+console.log(classs)
+console.log(classs.trim())
+const url="https//nikhil@25252.com/nikhil%20kumawat"
+console.log(url)
+console.log(url.replace('%20','-'))
+console.log(url.includes("nikhill"))
+const ui="hitesh-hii-ke hal hai"
+console.log(ui.split('-'))
