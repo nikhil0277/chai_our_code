d32a20fee519f3745858f08900168e59ba375532
nikhil0277 <nikhil07407@gmial.com>
2024-06-21 12:28:03 +0530

    commit

--- javascript/intro.js
+++ javascript/intro.js
@@ -4,4 +4,4 @@ var pass=3737
 console.table([id,pass,email])
 // const change nhi hota
 // let out var change ho skta hai
-// var basically blocks me use hota tha but ab nhi hota kyuki isse jo bhi declaired variables hn unme se kisi ek ko change kre to sare ho jayege 
+// var basically blocks me use hota tha but ab nhi hota kyuki isse jo bhi declaired variables hn unme se kisi ek ko change kre to sare ho jayege git 
