d906c8ee83ffaf2662a92956bdf6c03ed1992bf1
nikhil0277 <nikhil07407@gmial.com>
2024-06-21 22:40:36 +0530

    commit

---
+++ javascript/chai_and_code/numberAndMaths.js
@@ -0,0 +1,20 @@
+const num=400
+const newVariable=new Number(200)
+console.log(num)
+console.log(newVariable.toString().length)
+console.log(typeof newVariable)
+console.log(num.toFixed(2))
+const anyNumber=23.5445
+console.log(anyNumber.toPrecision(6))
+const lakh =10000000
+console.log(lakh.toLocaleString('en-IN'))
+// MATHS 
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(22.786))
+console.log(Math.ceil(9.856))
+console.log(Math.floor(299.99))
+console.log((Math.random()*20)+1)
+const Min=10
+const Max=20
+console.log(Math.floor(Math.random()*(Min-Max+1)+Min))
