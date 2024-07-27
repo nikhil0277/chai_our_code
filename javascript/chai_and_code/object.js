1496d9ffcbef0d8ac8616fa388da6cc09933998f
nikhil0277 <nikhil7407@gmail.com>
2024-07-27 08:51:25 +0530

    commit

---
+++ javascript/chai_and_code/object.js
@@ -0,0 +1,12 @@
+//singleton
+//object
+const jsUser={
+    name : "nikhil",
+    class : "btech2year",
+    gmail:"nikhil2434@gmail.com",
+    isLoggedIn : false,
+    lastLoginDays :["mon","sat"]
+}
+console.log(jsUser.name)//first way to acess an object's elements
+console.log(jsUser["name"])//second way to acess an object's element 
+console.log(jsUser)
